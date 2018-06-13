905e682725c73806d1256a6077ed9ace75a110cd
Kevin Bosteels <Kevin.Bosteels@ordina.be>
2018-06-13 22:33:11 +0200

    change module folder to components

--- bbtk/src/app/app.module.ts
+++ bbtk/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.module.routes';
+import { CardModule } from './components/card/card.module';
+import { CarouselModule } from './components/carousel/carousel.module';
+import { RepeatListModule } from './components/repeat-list/repeat-list.module';
 import { FooterComponent } from './footer/footer.component';
-import { CardModule } from './modules/card/card.module';
-import { CarouselModule } from './modules/carousel/carousel.module';
-import { RepeatListModule } from './modules/repeat-list/repeat-list.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ServicesComponent } from './pages/services/services.component';
-import { TruncatePipe } from './pipes/truncate.pipe';
 import { StarsPipe } from './pipes/stars.pipe';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
   declarations: [
